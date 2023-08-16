da98a6769d29fba520e1e1eec5b623550e0a555b
zhangxiancheng <365839159@qq.com>
2023-08-16 22:28:57 +0800

    添加测试样例

--- AbpvNextWebAPITemplate/content/src/Aquarius.Application.Contracts/AquariusApplicationContractsModule.cs
+++ AbpvNextWebAPITemplate/content/src/Aquarius.Application.Contracts/AquariusApplicationContractsModule.cs
@@ -1,4 +1,7 @@
-﻿using Volo.Abp.Modularity;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Volo.Abp.Modularity;
 using Volo.Abp.ObjectExtending;
 
 namespace Aquarius;
@@ -12,5 +15,10 @@ public class AquariusApplicationContractsModule : AbpModule
     public override void PreConfigureServices(ServiceConfigurationContext context)
     {
         AquariusDtoExtensions.Configure();
+    }
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
+        context.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
     }
 }
---
+++ AbpvNextWebAPITemplate/content/src/Aquarius.Application.Contracts/Validations/TestCreateValidator.cs
@@ -0,0 +1,14 @@
+﻿using Aquarius.Tests.Cmds;
+using FluentValidation;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.eShopOnContainers.Services.Ordering.API.Application.Validations;
+
+
+public class TestCreateValidator : AbstractValidator<Create>
+{
+    public TestCreateValidator(ILogger<TestCreateValidator> logger)
+    {
+        RuleFor(command => command.Name).NotEmpty().NotNull().Length(6,10);
+    }
+}
